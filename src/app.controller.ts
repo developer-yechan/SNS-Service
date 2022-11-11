c3e0dfc3f13b3c6fd770a569a408a8ed8c2bb4c5
KIMYECHAN <kyc5244@gmail.com>
2022-11-11 13:52:05 +0900

    불필요한 console.log 제거

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,7 +9,6 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    console.log(1111);
     return this.authService.login(req.user);
   }
   @Get()
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,9 +12,7 @@ export class AuthService {
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.userService.findOneByEmail(email);
-    console.log(user);
     const validatePassword = await bcrypt.compare(password, user.password);
-    console.log(validatePassword);
     if (user && validatePassword) {
       //password 제외한 user 정보 result에 담아줌
       const { password, ...result } = user;

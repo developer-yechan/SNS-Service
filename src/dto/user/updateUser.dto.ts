20ae2e9511bc70e0450c2d99d3767cdbb516db7c
KIMYECHAN <kyc5244@gmail.com>
2022-12-15 11:54:56 +0900

    Test : user service 테스트 코드 작성

--- src/dto/user/updateUser.dto.ts
+++ src/dto/user/updateUser.dto.ts
@@ -1,21 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { CreateUserDto } from './createUser.dto';
 import {
   IsEmail,
   IsMobilePhone,
   IsOptional,
-  IsPhoneNumber,
   IsString,
   Matches,
-  MaxLength,
-  MinLength,
 } from 'class-validator';
 //PartialType 쓰면 @ApiProperty는 상속이 안됨
 export class UpdateUserDto {
   @ApiProperty({ description: 'email' })
   @IsOptional()
   @IsEmail()
-  readonly email: string;
+  readonly email?: string;
 
   @ApiProperty({ description: 'password' })
   @IsOptional()
@@ -23,20 +19,20 @@ export class UpdateUserDto {
   @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,14}$/, {
     message: 'password too weak',
   })
-  readonly password: string;
+  readonly password?: string;
 
   @ApiProperty({ description: 'phone_number' })
   @IsOptional()
   @IsMobilePhone()
-  readonly phone_number: string;
+  readonly phone_number?: string;
 
   @ApiProperty({ description: 'name' })
   @IsOptional()
   @IsString()
-  readonly name: string;
+  readonly name?: string;
 
   @ApiProperty({ description: 'profile_image' })
   @IsOptional()
   @IsString()
-  readonly image: string;
+  readonly image?: string;
 }
--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { User } from 'src/entity/user.entity';
+import { findUsers } from './mockValue';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [UserService],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    userController = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(userController).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of User', async () => {
+      const result = findUsers;
+      userService.findAll = jest.fn().mockResolvedValue(findUsers);
+      expect(await userController.getUsers()).toBe(result);
+    });
   });
 });

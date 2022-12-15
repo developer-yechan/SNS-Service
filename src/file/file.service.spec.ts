03615e01d184dcadc815386bbc780dd30a7a125f
KIMYECHAN <kyc5244@gmail.com>
2022-12-15 15:42:41 +0900

    Test : file 테스트 작성 중, 파일 업로드 버그 수정

--- src/file/file.service.spec.ts
+++ src/file/file.service.spec.ts
@@ -1,18 +1,34 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { FileService } from './file.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { post } from 'src/post/mockValue';
+import { PostRepository } from 'src/post/post.repository';
+import { FileRepository } from './file.repository';
+import { FileService } from './file.service';
+import { files, uploadSuccess } from './mockValue';
 
-// describe('FileService', () => {
-//   let service: FileService;
+describe('FileService', () => {
+  let service: FileService;
+  let fileRepository: FileRepository;
+  let postRepository: PostRepository;
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [FileService],
-//     }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [FileService, FileRepository, PostRepository],
+    }).compile();
 
-//     service = module.get<FileService>(FileService);
-//   });
+    service = module.get<FileService>(FileService);
+    fileRepository = module.get<FileRepository>(FileRepository);
+    postRepository = module.get<PostRepository>(PostRepository);
+  });
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('uploadFile', () => {
+    it('upload success', async () => {
+      const result = uploadSuccess;
+      postRepository.findPost = jest.fn().mockResolvedValue(post);
+      expect(await service.uploadFile(1, files)).toBe(result);
+    });
+  });
+});
---
+++ src/file/mockValue.ts
@@ -0,0 +1,10 @@
+import { Readable } from 'stream';
+
+export const uploadSuccess = {
+  id: 21,
+  imageurl:
+    'https://post-imagebucket.s3.ap-northeast-2.amazonaws.com/KakaoTalk_20221009_193422897_01_1670229193535.jpg',
+  postid: 11,
+};
+
+export const files: Express.MulterS3.File[] = [];
